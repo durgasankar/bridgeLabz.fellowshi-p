9060fec65a00e63c8c54ed34ed164147bf3ddb19
durgasankar <durgasankar.raja500@gmail.com>
2019-12-05 15:40:43 +0530

    card Shuffling completed

---
+++ src/com/bridgeLabz/objectOrientedPrograms/cards/Playing.java
@@ -0,0 +1,17 @@
+package com.bridgeLabz.objectOrientedPrograms.cards;
+
+/**
+ * Implementation function of cardExecution class.
+ * 
+ * @author Durgasankar Mishra
+ * @created 2019-12-05
+ * @version 11.0.5
+ */
+public class Playing {
+	public static void main(String[] args) {
+		CardExecution execution = new CardExecution();
+		execution.shuffleCard();
+		execution.displayCard();
+	}
+
+}
