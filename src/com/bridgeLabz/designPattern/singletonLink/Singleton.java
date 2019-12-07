f3a07835da8a387fe185de9488b0d9a10dc3e575
durgasankar <durgasankar.raja500@gmail.com>
2019-12-07 18:19:05 +0530

    singleton design pattern

---
+++ src/com/bridgeLabz/designPattern/singletonLink/Singleton.java
@@ -0,0 +1,35 @@
+package com.bridgeLabz.designPattern.singletonLink;
+
+import java.io.Serializable;
+
+/**
+ * This is Single ton class example.
+ * 
+ * @author Durgasankar Mishra
+ * @created 2019-12-07
+ * @version 11.0.5
+ */
+@SuppressWarnings("serial")
+public class Singleton implements Serializable {
+	/**
+	 * just a static class type variable
+	 */
+	private static Singleton singleInstance = new Singleton();
+
+	/**
+	 * constructor as private so that restrict from creating objects.
+	 */
+	private Singleton() {
+		System.out.println("Private constructor");
+	}
+
+	/**
+	 * just a getter function to get Instance
+	 * 
+	 * @return Class type data.
+	 */
+	public static Singleton getInstance() {
+		return singleInstance;
+	}
+
+}
