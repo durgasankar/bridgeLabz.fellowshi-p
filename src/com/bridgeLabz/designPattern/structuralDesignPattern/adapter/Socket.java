cf04b078397e05fd394e17d3f03411616189f84a
durgasankar <durgasankar.raja500@gmail.com>
2019-12-17 12:41:52 +0530

    aaa

---
+++ src/com/bridgeLabz/designPattern/structuralDesignPattern/adapter/Socket.java
@@ -0,0 +1,20 @@
+package com.bridgeLabz.designPattern.structuralDesignPattern.adapter;
+
+/**
+ * Socket class has one method which returns new Object of Volt class.
+ * 
+ * @author Durgasankar Mishra
+ * @created 2019-12-10
+ * @version 11.0.5
+ */
+public class Socket {
+	/**
+	 * This function returns new Object of Volt class type whenever is called.
+	 * 
+	 * @return new Object of Volt class
+	 */
+	public Volt getVolt() {
+		return new Volt(120);
+	}
+
+}
